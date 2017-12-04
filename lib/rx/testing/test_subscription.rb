c5cebb2f5e0ae197c62f944ea0252bb7bb27425e
Bittrance <anders.qvist@burtcorp.com>
2017-12-04 23:46:01 +0100

    TestSubscription works fine as a struct instance and does not need ugly bigints

--- lib/rx/testing/test_subscription.rb
+++ lib/rx/testing/test_subscription.rb
@@ -3,20 +3,5 @@
 module Rx
 
   # Records information about subscriptions to and unsubscriptions from observable sequences.
-  class TestSubscription < Struct.new(:subscribe, :unsubscribe)
-
-    FIXNUM_MAX = Float::MAX.to_i
-
-    def initialize(subscribe, unsubscribe = FIXNUM_MAX)
-      super
-    end
-
-    def infinite?
-      unsubscribe == FIXNUM_MAX
-    end
-    
-    def to_s
-      "#{subscribe}, #{infinite? ? 'Infinite' : unsubscribe}"
-    end
-  end
+  TestSubscription = Struct.new(:subscribe, :unsubscribe)
 end
