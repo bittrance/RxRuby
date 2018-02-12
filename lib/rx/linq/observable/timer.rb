795a18a9a8436db8a39a80b4d8004473a3d6504f
Bittrance <anders.qvist@burtcorp.com>
2018-02-12 23:44:53 +0100

    Implement time-based timers (and simplify all implementations)

--- lib/rx/linq/observable/timer.rb
+++ lib/rx/linq/observable/timer.rb
@@ -10,15 +10,48 @@ module Rx
 
       if Time === due_time
         if period.nil?
-          observable_timer_date(due_time, scheduler)
+          AnonymousObservable.new do |observer|
+            scheduler.schedule_absolute(due_time,
+              lambda {
+                observer.on_next(0)
+                observer.on_completed
+              })
+          end
         else
-          observable_timer_date_and_period(due_time, period, scheduler)
+          AnonymousObservable.new do |observer|
+            count = 0
+            d = due_time
+            p = Scheduler.normalize(period)
+            scheduler.schedule_recursive_absolute(d, lambda {|this|
+              if p > 0
+                now = scheduler.now()
+                d = d + p
+                d <= now && (d = now + p)
+              end
+              observer.on_next(count)
+              count += 1
+              this.call(d)
+            })
+          end
         end
       else
         if period.nil?
-          observable_timer_time_span(due_time, scheduler)
+          AnonymousObservable.new do |observer|
+            scheduler.schedule_relative(Scheduler.normalize(due_time),
+              lambda {
+                observer.on_next(0)
+                observer.on_completed
+              })
+          end
         else
-          observable_timer_time_span_and_period(due_time, period, scheduler)
+          AnonymousObservable.new do |observer|
+            count = 0
+            scheduler.schedule_recursive_relative(due_time, lambda { |this|
+              observer.on_next(count)
+              count += 1
+              this.call(period)
+            })
+          end
         end
       end
     end
