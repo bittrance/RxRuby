c1adf08643964a1f3e0b9f9e451c88add0211233
Quest <quest@lysator.liu.se>
2018-02-26 13:05:01 +0100

    .multicast defaults to a transparent selector.

--- lib/rx/linq/observable/multicast.rb
+++ lib/rx/linq/observable/multicast.rb
@@ -2,6 +2,7 @@ module Rx
   module Observable
     def multicast(subject_or_subject_selector, selector = nil)
       if Proc === subject_or_subject_selector
+        selector ||= lambda { |c| c.connect ; c }
         AnonymousObservable.new do |observer|
           connectable = self.multicast(subject_or_subject_selector.call)
           CompositeSubscription.new [selector.call(connectable).subscribe(observer), self]
