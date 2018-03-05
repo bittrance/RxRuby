952d35de90f4ef3700fd395c2f66c739854b91f2
Bittrance <anders.qvist@burtcorp.com>
2018-03-05 23:40:33 +0100

    Tests for TestInterval presentation.

--- test/rx/linq/observable/test_time_interval.rb
+++ test/rx/linq/observable/test_time_interval.rb
@@ -1,12 +1,18 @@
 require 'test_helper'
 
+class TestTimeInterval < Minitest::Test
+  def test_to_s
+    assert_equal '(2)@(1)', Rx::TimeInterval.new(1, 2).to_s
+  end
+end
+
 class TestOperatorTimeInterval < Minitest::Test
   include Rx::MarbleTesting
 
   def test_wrap_value_with_delta_from_previous
     source      = cold('  1--2-3|')
-    expected    = msgs('--a--b-c|', 
-      a: Rx::TimeInterval.new(0, 1), 
+    expected    = msgs('--a--b-c|',
+      a: Rx::TimeInterval.new(0, 1),
       b: Rx::TimeInterval.new(300, 2),
       c: Rx::TimeInterval.new(200, 3))
     source_subs = subs('  ^     !')
