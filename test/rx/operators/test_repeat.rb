50e55923b76eeb04281a932de105c5d85cb4647d
Bittrance <anders.qvist@burtcorp.com>
2018-03-08 17:53:51 +0100

    WIP tests .repeat

--- test/rx/operators/test_repeat.rb
+++ test/rx/operators/test_repeat.rb
@@ -28,14 +28,7 @@ class TestCreationRepeat < Minitest::Test
 end
 
 class TestOperatorRepeat < Minitest::Test
-  include Rx::AsyncTesting
   include Rx::MarbleTesting
-
-  def setup
-    @scheduler = Rx::TestScheduler.new
-    @observer = @scheduler.create_observer
-    @err = RuntimeError.new
-  end
 
   def test_repeat
     source      = cold('  -12|')
@@ -66,30 +59,24 @@ class TestOperatorRepeat < Minitest::Test
       ).repeat(nil)
     end
   end
-  
+
   def test_repeat_infinitely
-    subscription = Rx::Observable.of(1, 2)
-      .repeat_infinitely
-      .subscribe_on(Rx::DefaultScheduler.instance)
-      .subscribe(@observer)
-    await_array_minimum_length(@observer.messages, 10)
-    subscription.unsubscribe
-    expected = ([
-      on_next(0, 1),
-      on_next(0, 2)
-    ] * 5).flatten
-    assert_messages expected, @observer.messages.take(10)
+    source      = cold('12|')
+    expected    = msgs('--12121212')
+    source_subs = subs('  ^ (!^) (!^) (!^) !')
+    actual = scheduler.configure { source.repeat_infinitely }
+    assert_msgs expected, actual
+    assert_subs source_subs, source
   end
-  
+
   def test_repeat_infinitely_breaks_on_error
-    res = @scheduler.configure do
-      @scheduler.create_cold_observable(
-        on_error(100, @err)
-      ).repeat_infinitely
-    end
-    expected = [
-      on_error(SUBSCRIBED + 100, @err)
-    ]
-    assert_messages expected, res.messages
+    source      = cold('  -12#')
+    expected    = msgs('---12#')
+    source_subs = subs('  ^  !')
+
+    actual = scheduler.configure { source.repeat_infinitely }
+
+    assert_msgs expected, actual
+    assert_subs source_subs, source
   end
 end
