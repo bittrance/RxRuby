e8ef3864578973f9457aef9cff5aa11b03b131b1
Quest <quest@lysator.liu.se>
2018-03-15 16:33:19 +0100

    await_criteria is expected to flunk on timeout.

--- lib/rx/testing/async_testing.rb
+++ lib/rx/testing/async_testing.rb
@@ -12,16 +12,25 @@ module Rx
     end
 
     def await_array_length(array, expected, timeout = 2)
-      return if await_criteria(timeout) { array.length == expected }
+      return if await(timeout) { array.length == expected }
       flunk "Array expected to be #{expected} items but was #{array}"
     end
 
     def await_array_minimum_length(array, expected, timeout = 2)
-      return if await_criteria(timeout) { array.length >= expected }
+      return if await(timeout) { array.length >= expected }
       flunk "Array expected to be at least #{expected} items but was #{array}"
     end
 
-    def await_criteria(timeout)
+    def await_criteria(timeout, failure = nil, &block)
+      unless await(timeout, &block)
+        failure ||= lambda { "Timed out after #{timeout} seconds" }
+        flunk failure.call
+      end
+    end
+
+    private
+
+    def await(timeout)
       deadline = Time.now + timeout
       while Time.now < deadline
         sleep timeout / 20
